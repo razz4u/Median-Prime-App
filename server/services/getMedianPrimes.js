6069f82197487854d3f80607944ce98a7c70b9cc
rajesh <rajeshjaiswal816@gmail.com>
2023-04-02 18:38:20 -0700

    Optimize getprime fun to check only odd nums

--- server/services/getMedianPrimes.js
+++ server/services/getMedianPrimes.js
@@ -1,45 +1,52 @@
 
 
 const getMedianPrimes = (n) => {
-    if(!Number.isInteger(n)){
+    if (!Number.isInteger(n)) {
         throw new Error("Incorrect input type")
     }
-    const primes =  getPrimes(n)
-    const medianprime =  getMedian(primes)
-    return medianprime      
+    if (n < 2) {
+        return []
+    }
+    const primes = getPrimes(n)
+    const medianprime = getMedian(primes)
+    return medianprime
 }
 
-function getPrimes(n){
-    let isPrime = new Array(n+1).fill(true);
+function getPrimes(n) {
+    let isPrime = new Array(n + 1).fill(true);
 
-    for(let i = 2; i*i<=n; i++){
-        if(isPrime[i]){
-            for (let j = i*i; j<=n; j += i){
+    isPrime[1] = false;
+
+    // only checking odd numbers, evens are non prime 
+    for (let i = 3; i * i <= n; i += 2) {
+        if (isPrime[i]) {
+            //only check odd nums
+            for (let j = i * i; j <= n; j += 2 * i) {
                 isPrime[j] = false;
             }
         }
     }
 
-    let primes = []
-    for(let i = 2; i<=n; i++){
-        if(isPrime[i]) {
+    let primes = [2]
+    for (let i = 3; i <= n; i += 2) {
+        if (isPrime[i]) {
             primes.push(i);
         }
     }
     return primes
 }
 
-function getMedian(primes){
-    let median =[];
-    if(primes.length === 0){
+function getMedian(primes) {
+    let median = [];
+    if (primes.length === 0) {
         return []
     }
-    if(primes.length % 2 === 0){
-        let mid = primes.length/2
-        median.push(primes[mid-1])
+    if (primes.length % 2 === 0) {
+        let mid = primes.length / 2
+        median.push(primes[mid - 1])
         median.push(primes[mid])
     } else {
-        median.push(primes[Math.floor(primes.length/2)])
+        median.push(primes[Math.floor(primes.length / 2)])
     }
 
     return median
